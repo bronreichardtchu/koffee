f10e794e6416eebf34bef634ab81ab18a1836799
bronreichardtchu <breichardtchu@swin.edu.au>
2021-07-29 17:39:53 +1000

    Created a main.py - still very empty!

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+NAME:
+	main.py
+
+AUTHOR:
+	Bronwyn Reichardt Chu
+	Swinburne
+	2021
+
+EMAIL:
+	<breichardtchu@swin.edu.au>
+
+PURPOSE:
+	To run through everything.
+	Written on MacOS Mojave 10.14.5, with Python 3.7
+
+"""
+import numpy as np
+
+import galaxy_class as gc
+
+def main(galaxy_dict):
+    """
+    Runs through all of the things.
+    """
+    #create the galaxy class
+    
